7301c426cc1870401a4fe76909b09735de6a955f
Jack Bodine <JackTaylorBodine@gmail.com>
2022-04-18 16:07:47 +0200

    Made a notebook to train Plato-Bot
    Trained for 1-epoch, took about 5 minutes.

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ import streamlit as st
 st.title("Drunk Philosophers")
 
 data_raw = pd.read_csv("data/philosophy_data.csv")
+data_raw = data_raw.drop(['sentence_spacy', 'original_publication_date', 'corpus_edition_date', 'sentence_length', 'sentence_lowered', 'lemmatized_str'], axis=1)
 philosophers = list(dict.fromkeys(data_raw['author'].tolist()))
 
 option = st.selectbox('Select a Philosopher', philosophers)
 
 selected_data = data_raw.loc[data_raw['author'] == option]
+text = ' '.join(selected_data['sentence_str'].tolist())
 
 st.dataframe(selected_data)
+
+st.write(text[:2000])
